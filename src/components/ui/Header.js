17f2017ca80d54ea6eced6d5152bc1316a14e730
su.park <su.park@kakaocorp.com>
2021-02-16 13:29:44 +0900

    Centralize the configuration of tab's font

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -20,10 +20,7 @@ const useStyles = makeStyles(theme => ({
     marginLeft: 'auto'
   },
   tab: {
-    fontFamily: "Raleway",
-    textTransform: "none",
-    fontWeight: 700,
-    fontSize: "1rem",
+    ...theme.typography.tab,
     minWidth: 10,
     marginLeft: "25px"
   }
