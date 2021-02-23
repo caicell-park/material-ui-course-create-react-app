a23df959b274c2181e10ec0e6642d978cac4a519
su.park <su.park@kakaocorp.com>
2021-02-23 13:37:29 +0900

    Fix the grammer for importing package

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import Header from '../components/ui/Header';
 import { ThemeProvider } from '@material-ui/styles';
-import theme from '../components/ui/Theme';
+import Header from './ui/Header';
+import theme from './ui/Theme';
 
 function App() {
-    return (
-        <ThemeProvider theme={theme}>
-            <Header />
-            Hello!
-        </ThemeProvider>
-    )
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+      Hello!
+    </ThemeProvider>
+  );
 }
 
 export default App;
