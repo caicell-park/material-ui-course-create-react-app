c171b9acb25c5e365d71a1e8a7d6b6847c98b39c
su.park <su.park@kakaocorp.com>
2021-02-24 20:16:12 +0900

    Add ListItem on Menu

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -13,6 +13,9 @@ import usedMediaQuery from '@material-ui/core/useMediaQuery';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 import logo from '../../assets/logo.svg';
 
@@ -187,35 +190,20 @@ export default function Header(props) {
         className={classes.tabContainer}
         indicatorColor="secondary"
       >
-        <Tab className={classes.tab} component={Link} to="/" label="Home" />
-        <Tab
-          aria-owns={anchorEl ? 'simple-menu' : undefined}
-          aria-haspopup={anchorEl ? true : undefined}
-          className={classes.tab}
-          component={Link}
-          onMouseOver={(event) => handleClick(event)}
-          to="/services"
-          label="Services"
-        />
-        <Tab
-          className={classes.tab}
-          component={Link}
-          to="/revolution"
-          label="The Revoultion"
-        />
-        <Tab
-          className={classes.tab}
-          component={Link}
-          to="/about"
-          label="About Us"
-        />
-        <Tab
-          className={classes.tab}
-          component={Link}
-          to="/contact"
-          label="Contact Us"
-        />
+        {routes.map((route) => (
+          <Tab
+            key={route.name}
+            className={classes.tab}
+            component={Link}
+            to={route.link}
+            label={route.name}
+            aria-owns={route.ariaOwns}
+            aria-haspopup={route.ariaPopup}
+            onMouseOver={route.mouseOver}
+          />
+        ))}
       </Tabs>
+
       <Button variant="contained" color="secondary" className={classes.button}>
         Free Estimate
       </Button>
@@ -257,7 +245,19 @@ export default function Header(props) {
         onClose={() => setOpenDrawer(false)}
         onOpen={() => setOpenDrawer(true)}
       >
-        Example Drawer
+        <List disablePadding>
+          {routes.map((route) => (
+            <ListItem
+              onClick={() => setOpenDrawer(false)}
+              divider
+              button
+              component={Link}
+              to={route.link}
+            >
+              <ListItemText disableTypography>{route.name}</ListItemText>
+            </ListItem>
+          ))}
+        </List>
       </SwipeableDrawer>
       <IconButton
         className={classes.drawerIconContainer}
@@ -268,20 +268,6 @@ export default function Header(props) {
       </IconButton>
     </>
   );
-
-  useEffect(() => {
-    if (window.location.pathname === '/' && value !== 0) {
-      setValue(0);
-    } else if (window.location.pathname === '/services' && value !== 1) {
-      setValue(1);
-    } else if (window.location.pathname === '/revolution' && value !== 2) {
-      setValue(2);
-    } else if (window.location.pathname === '/about' && value !== 3) {
-      setValue(3);
-    } else if (window.location.pathname === '/contact' && value !== 4) {
-      setValue(4);
-    }
-  }, [value]);
 
   return (
     <>
