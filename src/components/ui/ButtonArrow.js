49b6c0e53f5a0771e30a661adfbb3c0f12ec5176
su.park <su.park@kakaocorp.com>
2021-03-03 14:58:03 +0900

    Add description container

---
+++ src/components/ui/ButtonArrow.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function ButtonArrow(props) {
+  const { className, width, height, fill } = props;
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      fill={fill}
+      viewBox="0 0 18 18"
+    >
+      <path d="M9 3L7.94 4.06l4.19 4.19H3v1.5h9.13l-4.19 4.19L9 15l6-6z" />
+    </svg>
+  );
+}
+
+ButtonArrow.propTypes = {
+  className: PropTypes.string,
+  width: PropTypes.isRequired,
+  height: PropTypes.isRequired,
+  fill: PropTypes.isRequired,
+};
+
+ButtonArrow.defaultProps = {
+  className: undefined,
+};
