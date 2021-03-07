f2a53dd077ab4ab8f15664f7c0a78569974bf168
su.park <su.park@kakaocorp.com>
2021-03-07 13:58:10 +0900

    Add validation process for TextFields

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -83,8 +83,43 @@ export default function Contact(props) {
 
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [emailHelper, setEmailHelper] = useState('');
   const [phone, setPhone] = useState('');
+  const [phoneHelper, setPhoneHelper] = useState('');
   const [message, setMessage] = useState('');
+
+  const onChange = (event) => {
+    let valid;
+
+    switch (event.target.id) {
+      case 'email':
+        setEmail(event.target.value);
+        valid = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(
+          event.target.value,
+        );
+
+        if (!valid) {
+          setEmailHelper('Invalid Email');
+        } else {
+          setEmailHelper('');
+        }
+        break;
+      case 'phone':
+        setPhone(event.target.value);
+        valid = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(
+          event.target.value,
+        );
+
+        if (!valid) {
+          setPhoneHelper('Invalid Phone');
+        } else {
+          setPhoneHelper('');
+        }
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
     <Grid container direction="row">
@@ -133,7 +168,12 @@ export default function Contact(props) {
                   variant="body1"
                   style={{ color: theme.palette.common.blue, fontSize: '1rem' }}
                 >
-                  (555) 555-5555
+                  <a
+                    href="tel:5555555555"
+                    style={{ textDecoration: 'none', color: 'inherit' }}
+                  >
+                    (555) 555-5555
+                  </a>
                 </Typography>
               </Grid>
             </Grid>
@@ -150,7 +190,12 @@ export default function Contact(props) {
                   variant="body1"
                   style={{ color: theme.palette.common.blue, fontSize: '1rem' }}
                 >
-                  su-park@email.com
+                  <a
+                    href="mailto:sangeonpark92@gmail.com"
+                    style={{ textDecoration: 'none', color: 'inherit' }}
+                  >
+                    sangeonpark92@email.com
+                  </a>
                 </Typography>
               </Grid>
             </Grid>
@@ -173,18 +218,22 @@ export default function Contact(props) {
                 <TextField
                   label="Email"
                   id="email"
+                  error={emailHelper.length !== 0}
+                  helperText={emailHelper}
                   fullWidth
                   value={email}
-                  onChange={(event) => setEmail(event.target.value)}
+                  onChange={onChange}
                 />
               </Grid>
               <Grid item style={{ marginBottm: '0.5em' }}>
                 <TextField
                   label="Phone"
                   id="phone"
+                  error={phoneHelper.length !== 0}
+                  helperText={phoneHelper}
                   fullWidth
                   value={phone}
-                  onChange={(event) => setPhone(event.target.value)}
+                  onChange={onChange}
                 />
               </Grid>
             </Grid>
@@ -202,6 +251,12 @@ export default function Contact(props) {
             </Grid>
             <Grid item container justify="center">
               <Button
+                disabled={
+                  name.length === 0 ||
+                  message.length === 0 ||
+                  phoneHelper.length !== 0 ||
+                  emailHelper.length !== 0
+                }
                 variant="contained"
                 className={classes.sendButton}
                 style={{ marginTop: '2em' }}
